027cffd27d65c7cd18af2bdf6bf0fcaaf995513d
Antonio Queb <antonioqueb@alphaqueb.online>
2025-03-12 20:23:12 -0600

    Up

--- app/main.py
+++ app/main.py
@@ -65,5 +65,10 @@ def api_compras():
     return jsonify(data)
 
 
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({'status': 'running'}), 200
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
